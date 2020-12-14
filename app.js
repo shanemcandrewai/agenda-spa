091d2adad03944768eb6eab32db1b0ab501c6df1
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2020-12-14 15:46:13 +0100

    add db, passport.js

--- app.js
+++ app.js
@@ -1,77 +1,74 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const sqlite3 = require('sqlite3').verbose();
+const PORT = process.env.PORT || 5000
+const db = new sqlite3.Database('nodelogin.db');
 const session = require('express-session')
-const port = process.env.PORT || 3000 
+ 
 
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended: false }))
+const app = express();
+app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
   resave: false, // don't save session if unmodified
   saveUninitialized: false, // don't create session until something stored
   secret: 'shhhh, very secret'
 }));
 
-app.use(function(req, res, next){
-  var err = req.session.error;
-  console.log('sss ', err)
-  var msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = '';
-  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
-  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
-  next();
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
 });
 
-var users = {
-  tj: { name: 'tj' }
-};
-
-function authenticate(name, pass, fn) {
-  if (!module.parent) console.log('authenticating %s:%s', name, pass);
-  var user = users[name];
-  // query the db for the given username
-  if (!user) return fn(new Error('cannot find user'));
-  // apply the same algorithm to the POSTed password, applying
-  // the hash against the pass / salt, if there is a match we
-  // found the user
-  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
-    if (err) return fn(err);
-    if (hash === user.hash) return fn(null, user)
-    fn(new Error('invalid password'));
-  });
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
-
-app.post('/login', function(req, res){
-  authenticate(req.body.username, req.body.password, function(err, user){
-    if (user) {
-      // Regenerate session when signing in
-      // to prevent fixation
-      req.session.regenerate(function(){
-        // Store the user's primary key
-        // in the session store to be retrieved,
-        // or in this case the entire user object
-        req.session.user = user;
-        req.session.success = 'Authenticated as ' + user.name
-          + ' click to <a href="/logout">logout</a>. '
-          + ' You may now access <a href="/restricted">/restricted</a>.';
-        res.redirect('back');
-      });
-    } else {
-      req.session.error = 'Authentication failed, please check your '
-        + ' username and password.'
-        + ' (use "tj" and "foobar")';
-      res.redirect('/');
-    }
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
   });
 });
 
-app.listen(port, () => {
-  console.log(`agenda-spa app listening at http://localhost:${port}`)
-})
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+  
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+  
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+app.listen(PORT, () => console.log(`agenda-spa listening at http://localhost:${PORT}`))
